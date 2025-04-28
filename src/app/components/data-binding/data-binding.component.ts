1693ebd1e6fbbeb49c4366d34dff80c4b4fa5507
nikeshgadekar007 <nikeshgadekar@gmail.com>
2025-04-28 19:29:30 +0800

    first commit

---
+++ src/app/components/data-binding/data-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+
+  myName: string = "Nikesh"
+  isIndian: boolean = true;
+}
